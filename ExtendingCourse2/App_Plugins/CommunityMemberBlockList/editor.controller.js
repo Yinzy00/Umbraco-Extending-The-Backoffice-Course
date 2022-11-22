f74975b71bc3082b2b4fde79304f0abfd509ee78
Yinzy00 <yari.marien@telenet.be>
2022-11-22 14:58:08 +0100

    Add project files.

---
+++ ExtendingCourse2/App_Plugins/CommunityMemberBlockList/editor.controller.js
@@ -0,0 +1,21 @@
+﻿angular.module("umbraco")
+    .controller("communityMemberController", function ($scope, mediaResource) {
+
+        $scope.$watch('block.data', function () {
+            //your property is called image so the following will contain the udi
+            var imageUdi = $scope.block.data.image;
+            if (imageUdi !== null && imageUdi !== '') {
+
+                //the mediaResource has a getById method
+                mediaResource.getById(imageUdi[0].mediaKey)
+                    .then(function (media) {
+                        $scope.imageUrl = media.mediaLink;
+                    });
+            }
+        }, true);
+
+        $scope.$watch('block.settingsData', function () {
+            $scope.bgColor = $scope.block.settingsData.backgroundColor.label;
+            $scope.bgTextcolor = $scope.block.settingsData.textColor.label;
+        }, true);
+    });
---
+++ ExtendingCourse2/App_Plugins/forumpostsearcher/editor.controller.js
@@ -0,0 +1,26 @@
+﻿angular.module("umbraco")
+    .controller("forumpostEditorController", function ($scope, $http) {
+
+        if (angular.isArray($scope.model.value) === false) {
+            $scope.model.value = [];
+        }
+
+        $scope.search = function (term) {
+            if (term && term.length > 4) {
+                var url = "https://our.umbraco.com/umbraco/api/OurSearch/GetForumSearchResults?term=" + term;
+
+                $http.get(url).then(function (response) {
+                    $scope.model.results = response.data.items;
+                });
+            } else {
+                $scope.model.results = [];
+            }
+        };
+
+        $scope.add = function (result) {
+            $scope.model.value.push(result);
+        };
+        $scope.remove = function (index) {
+            $scope.model.value.splice(index, 1);
+        };
+    });
